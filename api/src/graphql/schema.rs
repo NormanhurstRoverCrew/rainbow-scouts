d20fc8faadedb9c781965d4baf40b425699569c9
Grant Perry <grant42perry@gmail.com>
2019-12-23 23:52:13 +1100

    graphQL mutation newOrder works pretty neat!

--- api/src/graphql/schema.rs
+++ api/src/graphql/schema.rs
@@ -1,14 +1,9 @@
-use juniper::{Context as JuniperContext, ID};
-use mongodb::{
-	coll::{options::WriteModel, Collection},
-	db::ThreadedDatabase,
-	oid::ObjectId,
-	Bson,
-};
+use juniper::{graphql_value, Context as JuniperContext, FieldResult, ID};
+use mongodb::{coll::Collection, db::ThreadedDatabase, oid::ObjectId, Bson};
 
 use crate::{
 	db::PrimaryDb,
-	models::{Purchase, Todo, User},
+	models::{Address, CollectionMethod, Order, User},
 };
 
 pub struct Context {
@@ -16,107 +11,71 @@ pub struct Context {
 }
 
 impl Context {
-	pub fn todos_handle(&self) -> Collection { self.connection.collection("todos") }
-	pub fn purchases_handel(&self) -> Collection { self.connection.collection("purchases") }
+	pub fn orders_handel(&self) -> Collection { self.connection.collection("orders") }
 }
 
 impl JuniperContext for Context {}
 
-#[juniper::object(description = "A todo item that can be marked as completed")]
-impl Todo {
-	/// The unique id of the todo item
-	fn id(&self) -> &ID { &self.id }
-
-	/// The user-editable title
-	fn title(&self) -> &str { &self.title }
-
-	/// Determines whether the user has completed the item or not
-	fn completed(&self) -> bool { self.completed }
-}
-
-#[juniper::object(description = "A Purchase holds the payment information for multiple tickets")]
+#[juniper::object(description = "A Order holds the payment information for multiple tickets")]
 impl User {
-	/// The unique id of the todo item
-	fn id(&self) -> &ID { &self.id }
-
 	/// The user-editable title
 	fn name(&self) -> &str { &self.name }
 
 	/// Determines whether the user has completed the item or not
-	fn email(&self) -> Option<String> { self.email.clone() }
-
-	/// Eh
-	fn mobile(&self) -> Option<String> { self.mobile.clone() }
-
-	/// Eh
-	fn crew(&self) -> Option<String> { self.crew.clone() }
+	fn email(&self) -> &str { &self.email }
 }
 
-#[juniper::object(description = "A Purchase holds the payment information for multiple tickets")]
-impl Purchase {
+#[juniper::object(description = "A Order holds the payment information for multiple tickets")]
+impl Order {
 	/// The unique id of the todo item
 	fn id(&self) -> &ID { &self.id }
 
 	/// The user-editable title
-	fn users(&self) -> Vec<User> { self.users.clone() }
+	fn user(&self) -> User { self.user.clone() }
+
+	/// The user-editable title
+	fn address(&self) -> Option<Address> { self.address.clone() }
+
+	/// The quantity of scarves to be orded
+	fn quantity(&self) -> i32 { self.quantity }
+
+	/// The quantity of scarves to be orded
+	fn method(&self) -> CollectionMethod { self.method }
+}
+
+#[juniper::object(description = "A Order holds the payment information for multiple tickets")]
+impl Address {
+	/// The user-editable title
+	fn apartment(&self) -> Option<String> { self.apartment.clone() }
+
+	/// The user-editable title
+	fn street(&self) -> &str { &self.street }
+
+	/// The quantity of scarves to be orded
+	fn town(&self) -> &str { &self.town }
+
+	/// The quantity of scarves to be orded
+	fn state(&self) -> &str { &self.state }
+
+	/// The quantity of scarves to be orded
+	fn post_code(&self) -> i32 { self.post_code }
 }
 
 pub struct QueryRoot;
-
 #[juniper::object(
     Context = Context,
 )]
 impl QueryRoot {
-	/// An array of every purchase
-	fn purchases(context : &Context) -> Vec<Purchase> {
-		let purchases = context.purchases_handel();
-		purchases
+	/// An array of every order
+	fn orders(context : &Context) -> Vec<Order> {
+		let orders = context.orders_handel();
+		orders
 			.find(None, None)
 			.unwrap()
 			.into_iter()
 			.filter_map(|item| match item {
-				Ok(item) => Some(Purchase::from_doc(item)),
+				Ok(item) => Some(Order::from_doc(item)),
 				Err(_) => None,
-			})
-			.collect()
-	}
-
-	/// An array of every user that has a ticket
-	fn users(context : &Context) -> Vec<User> {
-		let purchases = context.purchases_handel();
-		purchases
-			.find(None, None)
-			.unwrap()
-			.into_iter()
-			.filter_map(|item| match item {
-				Ok(item) => Some(Purchase::from_doc(item)),
-				Err(_) => None,
-			})
-			.flat_map(|item| {
-				item.users
-					.iter()
-					.map(|user| user.to_owned())
-					.collect::<Vec<User>>()
-			})
-			.collect()
-	}
-
-	/// Returns an array of all the crews that have a participant
-	fn crews(context : &Context) -> Vec<String> {
-		let purchases = context.purchases_handel();
-		purchases
-			.find(None, None)
-			.unwrap()
-			.into_iter()
-			.filter_map(|item| match item {
-				Ok(item) => Some(Purchase::from_doc(item)),
-				Err(_) => None,
-			})
-			.flat_map(|item| {
-				item.users
-					.iter()
-					.filter_map(|user| user.crew.clone())
-					.collect::<Vec<String>>()
 			})
 			.collect()
 	}
@@ -128,26 +87,81 @@ pub struct MutationRoot;
     Context = Context
 )]
 impl MutationRoot {
-	fn newPurchase(context : &Context, name : String) -> Option<Purchase> {
-		let purchases = context.purchases_handel();
-		let result = purchases
+	fn newOrder(
+		context : &Context,
+		name : String,
+		quantity : i32,
+		email : String,
+		address_apt : Option<String>,
+		address_street : Option<String>,
+		address_town : Option<String>,
+		address_state : Option<String>,
+		address_post_code : Option<i32>,
+		delivery_method : CollectionMethod,
+	) -> FieldResult<Option<Order>> {
+		if quantity < 1 {
+			return Err(juniper::FieldError::new(
+				"Quantity must be greater than 0",
+				graphql_value!({
+					"type": "NO_WHATEVER"
+				}),
+			));
+		};
+		let orders = context.orders_handel();
+
+		let result = orders
 			.insert_one(
 				doc! {
-					"users" => [{
-						"name" => name
-					}]
+						"quantity" => quantity,
+						"user" => {
+							"name" => name,
+							"email" => email,
+						},
+						"method" => match delivery_method {
+							CollectionMethod::Pickup => 1,
+							CollectionMethod::Post => 0,
+						} as i32,
 				},
 				None,
 			)
 			.unwrap();
 
+		match delivery_method {
+			CollectionMethod::Post => {
+				orders
+					.update_one(
+						doc! {"_id" => result.inserted_id.clone().unwrap() },
+						doc! {
+							"$set" => {
+								"address" => {
+									"apartment" => match address_apt {Some(a) => a, None => "".to_string()},
+									"street" => match address_street{Some(a) => a, None => "".to_string()},
+									"town" => match address_town{Some(a) => a, None => "".to_string()},
+									"state" => match address_state{Some(a) => a, None => "".to_string()},
+									"post_code" => match address_post_code{Some(a) => a, None => 0},
+								}
+							}
+						},
+						None,
+					)
+					.unwrap();
+			},
+			_ => {},
+		};
+
 		let id = result
 			.inserted_id
 			.unwrap_or(Bson::ObjectId(ObjectId::new().unwrap()));
 
-		match purchases.find_one(Some(doc! {"_id" => id}), None) {
-			Ok(Some(purchase)) => Some(Purchase::from_doc(purchase)),
-			_ => None,
+		match orders.find_one(Some(doc! {"_id" => id}), None) {
+			Ok(Some(order)) => Ok(Some(Order::from_doc(order))),
+			Ok(None) => Ok(None),
+			_ => Err(juniper::FieldError::new(
+				"Whatever does not exist",
+				graphql_value!({
+					"type": "NO_WHATEVER"
+				}),
+			)),
 		}
 	}
 }
--- api/src/models.rs
+++ api/src/models.rs
@@ -1,17 +1,22 @@
-use juniper::ID;
-use mongodb::{oid::ObjectId, Bson, Document};
-pub struct Todo {
-	pub id :        ID,
-	pub title :     String,
-	pub completed : bool,
+use juniper::{GraphQLEnum, ID};
+use mongodb::{oid::ObjectId, Document};
+
+pub struct Order {
+	pub id :       ID,
+	pub quantity : i32,
+	pub address :  Option<Address>,
+	pub user :     User,
+	pub method :   CollectionMethod,
 }
 
-impl Todo {
+impl Order {
 	pub fn from_doc(item : Document) -> Self {
 		Self {
-			id :        Self::doc_get_id(&item),
-			title :     Self::doc_get_title(&item),
-			completed : Self::doc_get_completed(&item),
+			id :       Self::doc_get_id(&item),
+			quantity : Self::doc_get_quantity(&item),
+			user :     Self::doc_get_user(&item),
+			address :  Self::doc_get_address(&item),
+			method :   Self::doc_get_method(&item),
 		}
 	}
 
@@ -22,80 +27,54 @@ impl Todo {
 		})
 	}
 
-	pub fn doc_get_title(item : &Document) -> String {
-		String::from(match item.get_str("title") {
-			Ok(t) => t,
-			_ => "",
-		})
-	}
-
-	pub fn doc_get_completed(item : &Document) -> bool {
-		match item.get_bool("completed") {
-			Ok(c) => c,
-			_ => false,
-		}
-	}
-}
-
-pub struct Purchase {
-	pub id :    ID,
-	pub users : Vec<User>,
-}
-
-impl Purchase {
-	pub fn from_doc(item : Document) -> Self {
-		Self {
-			id :    Self::doc_get_id(&item),
-			users : Self::doc_get_users(&item),
+	pub fn doc_get_quantity(item : &Document) -> i32 {
+		match item.get_i32("quantity") {
+			Ok(q) => q as i32,
+			_ => 0,
 		}
 	}
 
-	pub fn doc_get_id(item : &Document) -> ID {
-		ID::from(match item.get_object_id("_id") {
-			Ok(oid) => oid.to_string(),
-			_ => ObjectId::new().unwrap().to_string(),
-		})
+	pub fn doc_get_user(item : &Document) -> User {
+		match item.get_document("user") {
+			Ok(d) => User::from_doc(d.to_owned()),
+			_ => User::default(),
+		}
 	}
 
-	pub fn doc_get_users(item : &Document) -> Vec<User> {
-		match item.get_array("users") {
-			Ok(t) => t
-				.iter()
-				.filter_map(|i| match i {
-					Bson::Document(d) => Some(User::from_doc(d.to_owned())),
-					_ => None,
-				})
-				.collect(), //t.iter().map(|i| User::from_doc(i.to_owned())).collect(),
-			_ => Vec::new(),
+	pub fn doc_get_address(item : &Document) -> Option<Address> {
+		match item.get_document("address") {
+			Ok(d) => Some(Address::from_doc(d.to_owned())),
+			_ => None,
+		}
+	}
+
+	pub fn doc_get_method(item : &Document) -> CollectionMethod {
+		match item.get_i32("method") {
+			Ok(1) => CollectionMethod::Pickup,
+			Ok(0) | _ => CollectionMethod::Post,
 		}
 	}
 }
 
 #[derive(Clone, Debug)]
 pub struct User {
-	pub id :     ID,
-	pub name :   String,
-	pub email :  Option<String>,
-	pub mobile : Option<String>,
-	pub crew :   Option<String>,
+	pub name :  String,
+	pub email : String,
 }
 
 impl User {
-	pub fn from_doc(item : Document) -> Self {
+	pub fn default() -> Self {
 		Self {
-			id :     Self::doc_get_id(&item),
-			name :   Self::doc_get_name(&item),
-			email :  Self::doc_get_email(&item),
-			mobile : Self::doc_get_mobile(&item),
-			crew :   Self::doc_get_crew(&item),
+			name :  "".to_string(),
+			email : "".to_string(),
 		}
 	}
 
-	pub fn doc_get_id(item : &Document) -> ID {
-		ID::from(match item.get_object_id("_id") {
-			Ok(oid) => oid.to_string(),
-			_ => ObjectId::new().unwrap().to_string(),
-		})
+	pub fn from_doc(item : Document) -> Self {
+		Self {
+			name :  Self::doc_get_name(&item),
+			email : Self::doc_get_email(&item),
+		}
 	}
 
 	pub fn doc_get_name(item : &Document) -> String {
@@ -105,24 +84,82 @@ impl User {
 		})
 	}
 
-	pub fn doc_get_mobile(item : &Document) -> Option<String> {
-		match item.get_str("mobile") {
-			Ok(c) => Some(String::from(c)),
-			_ => None,
-		}
-	}
-
-	pub fn doc_get_email(item : &Document) -> Option<String> {
+	pub fn doc_get_email(item : &Document) -> String {
 		match item.get_str("email") {
-			Ok(c) => Some(String::from(c)),
+			Ok(c) => String::from(c),
+			_ => String::from(""),
+		}
+	}
+}
+
+#[derive(Clone, Debug)]
+pub struct Address {
+	pub apartment : Option<String>,
+	pub street :    String,
+	pub town :      String,
+	pub state :     String,
+	pub post_code : i32,
+}
+
+impl Address {
+	pub fn default() -> Self {
+		Self {
+			apartment : None,
+			street :    "".to_string(),
+			town :      "".to_string(),
+			state :     "".to_string(),
+			post_code : 0,
+		}
+	}
+
+	pub fn from_doc(item : Document) -> Self {
+		Self {
+			apartment : Self::doc_get_apartment(&item),
+			street :    Self::doc_get_street(&item),
+			town :      Self::doc_get_town(&item),
+			state :     Self::doc_get_state(&item),
+			post_code : Self::doc_get_post_code(&item),
+		}
+	}
+
+	pub fn doc_get_apartment(item : &Document) -> Option<String> {
+		match item.get_str("apartment") {
+			Ok(t) => Some(String::from(t)),
 			_ => None,
 		}
 	}
 
-	pub fn doc_get_crew(item : &Document) -> Option<String> {
-		match item.get_str("crew") {
-			Ok(c) => Some(String::from(c)),
-			_ => None,
+	pub fn doc_get_street(item : &Document) -> String {
+		match item.get_str("street") {
+			Ok(c) => String::from(c),
+			_ => String::from(""),
 		}
 	}
+
+	pub fn doc_get_town(item : &Document) -> String {
+		match item.get_str("town") {
+			Ok(c) => String::from(c),
+			_ => String::from(""),
+		}
+	}
+
+	pub fn doc_get_state(item : &Document) -> String {
+		match item.get_str("state") {
+			Ok(c) => String::from(c),
+			_ => String::from(""),
+		}
+	}
+
+	pub fn doc_get_post_code(item : &Document) -> i32 {
+		match item.get_i32("postcode") {
+			Ok(c) => c,
+			_ => 0,
+		}
+	}
+}
+
+#[derive(GraphQLEnum, Clone, Copy, Debug)]
+pub enum CollectionMethod {
+	Pickup,
+	Post,
 }
