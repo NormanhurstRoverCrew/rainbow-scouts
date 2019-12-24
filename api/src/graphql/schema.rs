ff4728d96b242bfdd5edcb5d2bd5635af5a8a306
Grant Perry <grant42perry@gmail.com>
2019-12-24 16:15:17 +1100

    GraphQL api should hopefully cater for the frontend

--- api/src/graphql/schema.rs
+++ api/src/graphql/schema.rs
@@ -3,8 +3,10 @@ use mongodb::{coll::Collection, db::ThreadedDatabase, oid::ObjectId, Bson};
 
 use crate::{
 	db::PrimaryDb,
-	models::{Address, CollectionMethod, Order, User},
+	models::{Address, CollectionMethod, Order, PostDeliveryOption, Postage, User},
 };
+
+const SCARVE_PRICE : f64 = 30.0;
 
 pub struct Context {
 	pub connection : PrimaryDb,
@@ -16,49 +18,64 @@ impl Context {
 
 impl JuniperContext for Context {}
 
-#[juniper::object(description = "A Order holds the payment information for multiple tickets")]
+#[juniper::object(description = "Contact Details of the person making the purchase")]
 impl User {
-	/// The user-editable title
+	/// Contact name
 	fn name(&self) -> &str { &self.name }
 
-	/// Determines whether the user has completed the item or not
+	/// Contact email
 	fn email(&self) -> &str { &self.email }
 }
 
-#[juniper::object(description = "A Order holds the payment information for multiple tickets")]
+#[juniper::object(
+	description = "The root order. This holds all details on an order including contact, address and postage information"
+)]
 impl Order {
-	/// The unique id of the todo item
 	fn id(&self) -> &ID { &self.id }
 
-	/// The user-editable title
+	/// Contact details
 	fn user(&self) -> User { self.user.clone() }
 
-	/// The user-editable title
+	/// delivery address
 	fn address(&self) -> Option<Address> { self.address.clone() }
 
-	/// The quantity of scarves to be orded
+	/// postage details
+	fn postage(&self) -> Option<Postage> { self.postage.clone() }
+
+	/// quantity of scarves to be delivered
 	fn quantity(&self) -> i32 { self.quantity }
 
-	/// The quantity of scarves to be orded
+	/// is the item Picked up or delivered
 	fn method(&self) -> CollectionMethod { self.method }
 }
 
-#[juniper::object(description = "A Order holds the payment information for multiple tickets")]
+#[juniper::object(description = "Delivery Address")]
 impl Address {
-	/// The user-editable title
 	fn apartment(&self) -> Option<String> { self.apartment.clone() }
 
-	/// The user-editable title
 	fn street(&self) -> &str { &self.street }
 
-	/// The quantity of scarves to be orded
 	fn town(&self) -> &str { &self.town }
 
-	/// The quantity of scarves to be orded
 	fn state(&self) -> &str { &self.state }
 
-	/// The quantity of scarves to be orded
 	fn post_code(&self) -> i32 { self.post_code }
+}
+
+#[juniper::object(description = "The type of postage the user has selected, and the price")]
+impl Postage {
+	fn typ(&self) -> &str { &self.typ }
+
+	fn price(&self) -> f64 { self.price }
+}
+
+#[juniper::object(description = "A post delivery option from Australia Post")]
+impl PostDeliveryOption {
+	/// The name of the delivery option
+	fn name(&self) -> &str { &self.name }
+
+	/// The price of the deliver option as stated by the API
+	fn price(&self) -> f64 { self.price }
 }
 
 pub struct QueryRoot;
@@ -66,7 +83,7 @@ pub struct QueryRoot;
     Context = Context,
 )]
 impl QueryRoot {
-	/// An array of every order
+	/// All orders
 	fn orders(context : &Context) -> Vec<Order> {
 		let orders = context.orders_handel();
 		orders
@@ -79,6 +96,90 @@ impl QueryRoot {
 			})
 			.collect()
 	}
+
+	/// For an order, calculate the price to post the items to the user
+	fn calculatePostage(context : &Context, id : String) -> FieldResult<Vec<PostDeliveryOption>> {
+		let orders = context.orders_handel();
+		let order = match orders.find_one(
+			Some(doc! {
+				"_id" => match mongodb::oid::ObjectId::with_string(&id) {
+					Ok(oid) => oid,
+					Err(_) => return Err(juniper::FieldError::new(
+					"UID is not valid",
+					graphql_value!({
+						"type": "INVALID_UID"
+					}),
+				))
+
+				}
+			}),
+			None,
+		) {
+			Ok(Some(o)) => Order::from_doc(o),
+			_ => {
+				return Err(juniper::FieldError::new(
+					"Internal Error decoding Document from database",
+					graphql_value!({
+						"type": "DATABASE_ERROR"
+					}),
+				))
+			},
+		};
+
+		let postcode = match order.address {
+			Some(addr) => addr.post_code,
+			None => {
+				return Err(juniper::FieldError::new(
+					"This order does not have an address defined. This is likely because the Pickup option was selected",
+					graphql_value!({
+						"type": "NO_ADDRESS"
+					}),
+				))
+			},
+		};
+
+		match PostDeliveryOption::get(order.quantity as u32, postcode as u32) {
+			Ok(opts) => Ok(opts),
+			Err(_) => Err(juniper::FieldError::new(
+				"Quantity must be greater than 0",
+				graphql_value!({
+					"type": "INVALID_QUANTITY"
+				}),
+			)),
+		}
+	}
+
+	/// Return the price of the order, excluding postage
+	fn orderPrice(context : &Context, id : String) -> FieldResult<f64> {
+		let orders = context.orders_handel();
+		let order = match orders.find_one(
+			Some(doc! {
+				"_id" => match mongodb::oid::ObjectId::with_string(&id) {
+					Ok(oid) => oid,
+					Err(_) => return Err(juniper::FieldError::new(
+					"UID is not valid",
+					graphql_value!({
+						"type": "NO_WHATEVER"
+					}),
+				))
+
+				}
+			}),
+			None,
+		) {
+			Ok(Some(o)) => Order::from_doc(o),
+			_ => {
+				return Err(juniper::FieldError::new(
+					"Internal Error decoding Document from database",
+					graphql_value!({
+						"type": "NO_WHATEVER"
+					}),
+				))
+			},
+		};
+
+		Ok(order.quantity as f64 * SCARVE_PRICE)
+	}
 }
 
 pub struct MutationRoot;
@@ -87,6 +188,8 @@ pub struct MutationRoot;
     Context = Context
 )]
 impl MutationRoot {
+	/// Take in the details of a user, how they would like to receive their
+	/// order and possibly their address.
 	fn newOrder(
 		context : &Context,
 		name : String,
@@ -112,15 +215,15 @@ impl MutationRoot {
 		let result = orders
 			.insert_one(
 				doc! {
-						"quantity" => quantity,
-						"user" => {
-							"name" => name,
-							"email" => email,
-						},
-						"method" => match delivery_method {
-							CollectionMethod::Pickup => 1,
-							CollectionMethod::Post => 0,
-						} as i32,
+				"quantity" => quantity,
+				"user" => {
+					"name" => name,
+					"email" => email,
+				},
+				"method" => match delivery_method {
+					CollectionMethod::Pickup => 1,
+					CollectionMethod::Post => 0,
+				} as i32,
 				},
 				None,
 			)
@@ -162,6 +265,65 @@ impl MutationRoot {
 					"type": "NO_WHATEVER"
 				}),
 			)),
+		}
+	}
+
+	/// Set the requested postage method from the user. Once this is done the
+	/// order is practically finalized and just needs to be paid for.
+	fn setPostage(
+		context : &Context,
+		id : String,
+		typ : String,
+		price : f64,
+	) -> FieldResult<Order> {
+		let orders = context.orders_handel();
+
+		orders
+			.update_one(
+				doc! {
+				"_id" => match mongodb::oid::ObjectId::with_string(&id) {
+					Ok(oid) => oid,
+					Err(_) => return Err(juniper::FieldError::new(
+					"UID is not valid",
+					graphql_value!({
+						"type": "NO_WHATEVER"
+					}),
+				))
+				}},
+				doc! {
+					"$set" => {
+						"postage" => {
+							"type" => typ,
+							"price" => price,
+						}
+					}
+				},
+				None,
+			)
+			.unwrap();
+
+		match orders.find_one(
+			Some(doc! {
+			"_id" => match mongodb::oid::ObjectId::with_string(&id) {
+				Ok(oid) => oid,
+				Err(_) => return Err(juniper::FieldError::new(
+				"UID is not valid",
+				graphql_value!({
+					"type": "NO_WHATEVER"
+				}),
+			))
+			}}),
+			None,
+		) {
+			Ok(Some(order)) => Ok(Order::from_doc(order)),
+			_ => {
+				return Err(juniper::FieldError::new(
+					"UID is not valid",
+					graphql_value!({
+						"type": "NO_WHATEVER"
+					}),
+				))
+			},
 		}
 	}
 }
--- api/src/models.rs
+++ api/src/models.rs
@@ -1,5 +1,7 @@
 use juniper::{GraphQLEnum, ID};
 use mongodb::{oid::ObjectId, Document};
+use reqwest::header;
+use serde::Deserialize;
 
 pub struct Order {
 	pub id :       ID,
@@ -7,6 +9,7 @@ pub struct Order {
 	pub address :  Option<Address>,
 	pub user :     User,
 	pub method :   CollectionMethod,
+	pub postage :  Option<Postage>,
 }
 
 impl Order {
@@ -17,6 +20,7 @@ impl Order {
 			user :     Self::doc_get_user(&item),
 			address :  Self::doc_get_address(&item),
 			method :   Self::doc_get_method(&item),
+			postage :  Self::doc_get_postage(&item),
 		}
 	}
 
@@ -44,6 +48,13 @@ impl Order {
 	pub fn doc_get_address(item : &Document) -> Option<Address> {
 		match item.get_document("address") {
 			Ok(d) => Some(Address::from_doc(d.to_owned())),
+			_ => None,
+		}
+	}
+
+	pub fn doc_get_postage(item : &Document) -> Option<Postage> {
+		match item.get_document("postage") {
+			Ok(d) => Some(Postage::from_doc(d.to_owned())),
 			_ => None,
 		}
 	}
@@ -151,9 +162,45 @@ impl Address {
 	}
 
 	pub fn doc_get_post_code(item : &Document) -> i32 {
-		match item.get_i32("postcode") {
+		match item.get_i32("post_code") {
 			Ok(c) => c,
 			_ => 0,
+		}
+	}
+}
+
+#[derive(Clone, Debug)]
+pub struct Postage {
+	pub typ :   String,
+	pub price : f64,
+}
+
+impl Postage {
+	pub fn default() -> Self {
+		Self {
+			typ :   String::from(""),
+			price : 0.0,
+		}
+	}
+
+	pub fn from_doc(item : Document) -> Self {
+		Self {
+			typ :   Self::doc_get_type(&item),
+			price : Self::doc_get_price(&item),
+		}
+	}
+
+	pub fn doc_get_type(item : &Document) -> String {
+		match item.get_str("type") {
+			Ok(c) => String::from(c),
+			_ => String::from(""),
+		}
+	}
+
+	pub fn doc_get_price(item : &Document) -> f64 {
+		match item.get_f64("price") {
+			Ok(c) => c,
+			_ => 0.0,
 		}
 	}
 }
@@ -162,4 +209,86 @@ impl Address {
 pub enum CollectionMethod {
 	Pickup,
 	Post,
+}
+
+#[derive(Deserialize, Debug)]
+struct PostPricesServiceOptions {
+	pub option : Vec<PostPricesService>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PostPricesService {
+	pub code :            String,
+	pub name :            String,
+	pub max_extra_cover : Option<u32>,
+	pub options :         Option<PostPricesServiceOptions>,
+	pub price :           Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PostPricesServices {
+	pub service : Vec<PostPricesService>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PostPrices {
+	pub services : PostPricesServices,
+}
+
+pub struct PostDeliveryOptions {
+	pub options : Vec<PostDeliveryOption>,
+}
+
+pub struct PostDeliveryOption {
+	pub name :  String,
+	pub price : f64,
+}
+
+pub enum PostDeliveryOptionError {
+	ApiError,
+}
+
+impl PostDeliveryOption {
+	pub fn get(quantity : u32, postcode : u32) -> Result<Vec<Self>, PostDeliveryOptionError> {
+		let mut headers = header::HeaderMap::new();
+		headers.insert(
+			header::HeaderName::from_static("auth-key"),
+			header::HeaderValue::from_str(&std::env::var("AUSPOST_PAC_API").unwrap()).unwrap(),
+		);
+		let client = reqwest::blocking::Client::builder()
+			.default_headers(headers)
+			.build()
+			.unwrap();
+		let body : PostPrices = match client
+			.get("https://digitalapi.auspost.com.au/postage/parcel/domestic/service.json")
+			.query(&[
+				("from_postcode", "2000"),
+				("to_postcode", &postcode.to_string()),
+				("length", "22"),
+				("width", "16"),
+				("height", "7.7"),
+				("weight", &(quantity as f64 * 0.2).to_string()),
+			])
+			.send()
+		{
+			Ok(response) => response.json().unwrap(),
+			Err(_) => return Err(PostDeliveryOptionError::ApiError),
+		};
+
+		Ok(body
+			.services
+			.service
+			.iter()
+			.map(|serv| PostDeliveryOption::from_api_service(serv))
+			.collect())
+	}
+	fn from_api_service(service : &PostPricesService) -> Self {
+		Self {
+			name :  service.name.to_owned(),
+			price : match &service.price {
+				Some(p) => p.parse::<f64>().unwrap(),
+				None => 0.0,
+			},
+		}
+	}
 }
